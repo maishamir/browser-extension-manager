35efdeb93504c895112cd3bdd4e793f557b64bf8
maishamir <45680002+maishamir@users.noreply.github.com>
2025-06-01 12:47:43 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      Extensions List
+
+      All
+      Active
+      Inactive
+
+      DevLens
+      Quickly inspect page layouts and visualize element boundaries.
+      Remove
+
+      StyleSpy
+      Instantly analyze and copy CSS from any webpage element.
+      Remove
+      
+      SpeedBoost
+      Optimizes browser resource usage to accelerate page loading.
+      Remove
+
+      JSONWizard
+      Formats, validates, and prettifiies JSON responses in-browser.
+      Remove
+
+      TabMaster Pro
+      Organizes browsser tabs into groups and sessions.
+      Remove
+
+      ViewportBuddy
+      Simulates various screen resolutions directly within the browser.
+      Remove
+
+      Markup Notes
+      Enables annotation and notes directly onto webpages for collaborative debugging.
+      Remove
+
+      GridGuides
+      Overlay customizable grids and alignment guides on any webpage.
+      Remove
+
+      Palette Picker
+      Instantly extracts color palettes from any webpage.
+      Remove
+
+      Dom Snapshot
+      Capture and export DOM structures quickly.
+      Remove
+
+      ConsolePlus
+      Enhanced developer console with advanced filtering and logging.
+      Remove
+    </>
+  )
+}
+
+export default App
